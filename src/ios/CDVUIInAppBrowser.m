e9795026cf8dff47d4d102eb12e88112d6f6f2eb
Ozge Ekemen <ekemen@gmail.com>
2019-10-25 15:46:31 +0200

    Update CDVUIInAppBrowser.m

--- src/ios/CDVUIInAppBrowser.m
+++ src/ios/CDVUIInAppBrowser.m
@@ -775,7 +775,7 @@ static CDVUIInAppBrowser* instance = nil;
         [self.toolbar setItems:@[self.closeButton, flexibleSpaceButton, self.backButton, fixedSpaceButton, self.forwardButton]];
     }
 
-    self.view.backgroundColor = [UIColor grayColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.toolbar];
     [self.view addSubview:self.addressLabel];
     [self.view addSubview:self.spinner];
